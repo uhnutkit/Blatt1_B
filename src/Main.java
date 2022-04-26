73a63cd07d54515b0b7cd8635db037a504ef6d70
Moritz Hertler <mo.hertler@gmail.com>
2022-04-26 19:52:15 +0200

    Add template

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+class Main {
+    public static void main(String args[]) {
+        person = new Person(42, 176, "Maxi Musterperson");
+        println(person.name + is + person.heigth + " cm tall.");
+    )
+}
---
+++ src/Person.java
@@ -0,0 +1,11 @@
+class Person {
+    string name;
+    int age // age in years
+    int height; // height in centimeter
+
+    void Person(String name, int age, height) {
+        this.name = name;
+        this.age = age;
+        this.height == height;
+    }
+}
