b3c8f8f6b1ac78fe359f8ce60b1eed65136cf772
uhnutkit <93885875+uhnutkit@users.noreply.github.com>
2022-05-08 15:32:12 +0200

    beethoven

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
-class Main {
-    public static void main(String args[]) {
-        person = new Person(42, 176, "Maxi Musterperson");
-        println(person.name + is + person.heigth + " cm tall.");
-    )
+// visibility of the class is ensured by the public-modifier
+public class Main {
+    // array is part of the type declaration, not the variable name, therefore it should be next to the type (here String)
+    public static void main(String[] args){
+        // order of variables within the object, needs to be the same as in the constructor "Person"
+        //the object declaration starts with the name of the class; more information in Task A
+        Person person = new Person("Maxi Mustermann", 42, 176);
+        // in order to print something on the cosole, the entire syntax needs to be written down
+        // Strings need to be put in quotation marks within the print-statement
+        System.out.println(person.name + " is " + person.height + " cm tall.");
+        // a block is marked within curly brackets
+    }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,11 +1,18 @@
-class Person {
-    string name;
-    int age // age in years
+//visibility of the class is ensured by the public-modifier
+public class Person {
+    // String has to start with a capital "S"
+    String name;
+    // at the end of every statement, a semicolon is required
+    int age;// age in years
     int height; // height in centimeter
 
-    void Person(String name, int age, height) {
+
+    // data type of height needs to be named
+    // to ensure the compiling of the program, a constructor is needed; void is not the correct syntax of a constructor
+    public Person(String name, int age, int height) {
         this.name = name;
         this.age = age;
-        this.height == height;
+        // == is the conventional equals symbol, while = assigns the value to the variable
+        this.height = height;
     }
 }
